45f1e159355c6096cce1c497c114ca94ae9f92f8
Risto Tõldsep <r.toldsep@gmail.com>
2022-10-30 23:19:45 +0200

    Displayed user and video thumbnail

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -16,6 +16,8 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
 
     //   console.log(post.caption);
 
+    const [isHover, setIsHover] = useState(false)
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
         <div>
@@ -34,6 +36,34 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                         </>
                     </Link>
                 </div>
+                <div>
+                    <Link href="/">
+                        <div className="flex items-center gap-2">
+                            <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                                {post.postedBy.userName} {` `}
+                                <GoVerified className="text-blue-400 text-md" />
+                            </p>
+                            <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">{post.postedBy.userName}</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+        <div className="lg:ml-20 flex gap-4 relative">
+            <div 
+                onMouseEnter={() => setIsHover(true)}
+                onMouseLeave={() => setIsHover(false)}
+            className="rounded-3xl">
+                <Link href="/">
+                    <video
+                        src={post.video.asset.url}
+                        loop
+                        className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+                    >
+
+                    </video>
+                </Link>
             </div>
         </div>
     </div>
