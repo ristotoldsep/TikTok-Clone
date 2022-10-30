b011df9026fc0f401b2ba5d7856c38ddffd54b0f
Risto Tõldsep <r.toldsep@gmail.com>
2022-10-31 00:18:26 +0200

    Got basic videocard working

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -2,72 +2,116 @@ import React, { useState, useEffect, useRef } from "react";
 import { Video } from "../types";
 import { NextPage } from "next";
 import Image from "next/image";
-import Link from 'next/link'
-import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
-import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs'
-import { GoVerified } from 'react-icons/go'
+import Link from "next/link";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
 
 interface IProps {
   post: Video;
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
+  //   console.log(post.caption);
 
+  // Hover state for video buttons
+  const [isHover, setIsHover] = useState(false);
 
-    //   console.log(post.caption);
+  // State for play button
+  const [playing, setPlaying] = useState(false);
 
-    const [isHover, setIsHover] = useState(false)
+  // State for volume button (Mute/unmute)
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+
+  // Using REF to play video
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  };
 
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
-        <div>
-            <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
-                <div className="md:w-16 md:h-16 w-10 h-10">
-                    <Link href="/">
-                        <>
-                            <Image
-                                width={62}
-                                height={62}
-                                className="rounded-full"
-                                src={post.postedBy.image}
-                                alt="profile photo"
-                                layout="responsive"
-                            />
-                        </>
-                    </Link>
-                </div>
-                <div>
-                    <Link href="/">
-                        <div className="flex items-center gap-2">
-                            <p className="flex gap-2 items-center md:text-md font-bold text-primary">
-                                {post.postedBy.userName} {` `}
-                                <GoVerified className="text-blue-400 text-md" />
-                            </p>
-                            <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">{post.postedBy.userName}</p>
-                        </div>
-                    </Link>
-                </div>
-            </div>
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              <div className="flex items-center gap-2">
+                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                  {post.postedBy.userName} {` `}
+                  <GoVerified className="text-blue-400 text-md" />
+                </p>
+                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+          {post.caption}
         </div>
+      </div>
 
-        <div className="lg:ml-20 flex gap-4 relative">
-            <div 
-                onMouseEnter={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}
-            className="rounded-3xl">
-                <Link href="/">
-                    <video
-                        src={post.video.asset.url}
-                        loop
-                        className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
-                    >
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className="rounded-3xl"
+        >
+          <Link href="/">
+            <video
+              ref={videoRef}
+              src={post.video.asset.url}
+              loop
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+            ></video>
+          </Link>
 
-                    </video>
-                </Link>
+          {isHover && (
+            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-20 lg:justify-between w-[100px] md:w-[50px] lg:w-[600px] py-3 px-7">
+              {playing ? (
+                <button onClick={onVideoPress}>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={onVideoPress}>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+              {isVideoMuted ? (
+                <button onClick={() => setIsVideoMuted(false)}>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={() => setIsVideoMuted(true)}>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
             </div>
+          )}
         </div>
+      </div>
     </div>
-    )   
+  );
 };
 
 export default VideoCard;
