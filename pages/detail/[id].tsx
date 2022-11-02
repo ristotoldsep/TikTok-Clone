57de3ca898098e585419c0e071eeb5e8b1b999a6
Risto Tõldsep <r.toldsep@gmail.com>
2022-11-02 03:02:56 +0200

    Started on video detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import axios from 'axios';
 
 const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
-}
+  return <div>Detail</div>;
+};
 
-export default Detail
+// TO GET DATA FOR DETAIL VIDEO PAGE, WE TAKE IT FROM THE URL AND use Server Side Props!
+
+
+export default Detail;
